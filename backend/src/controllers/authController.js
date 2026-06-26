8caa28d39c129f7e76d22ef7a205a1c565f07f34
Meghanaavva <mavva@gitam.in>
2026-06-26 21:47:30 +0530

    fix: lowercase user model filename and import

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-import User from '../models/User.js'
+import User from '../models/user.js'
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN || '7d' })
